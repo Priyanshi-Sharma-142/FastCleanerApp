5c7b52ddb158d4aa5604d3f0dc2a216855706f39
Priyanshi-Sharma-142 <priyanshisharma261@gmail.com>
2021-05-25 10:48:20 +0530

    Added changes

---
+++ app/src/main/java/com/example/files_cleaner/Application.java
@@ -0,0 +1,16 @@
+package com.example.files_cleaner;
+
+
+/*public class Application extends android.app.Application {
+
+    private static final String AD_ID = "ca-app-pub-7255339257613393~4556179800";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        MobileAds.initialize(this,
+                AD_ID);
+    }
+}
+*/
